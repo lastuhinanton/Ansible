349037d920a6295d0773549d7efa8644c82eb8f1
lastuhinanton <lastuhinanton74@gmail.com>
2023-05-08 13:13:40 +0300

    update file structure

--- ans_containers/config/initansible.py
+++ ans_containers/config/initansible.py
@@ -7,7 +7,7 @@ from time import sleep
 def initansible(count, name):
     with open(f"file.tmp", "wb") as f:
         for i in range(1, count+1):
-            subprocess.run(['docker', 'run', '--privileged', '-itd', f'--name={name}_{i}', 'managed'], stdout=subprocess.PIPE)
+            subprocess.run(['docker', 'run', '-itd', f'--name={name}_{i}', 'managed'], stdout=subprocess.PIPE)
             subprocess.check_call(['docker', 'inspect', '-f', '{{ .NetworkSettings.IPAddress }}', f'{name}_{i}'], stdout=f)
             print(i); sleep(0.1); os.system('clear')
         print(f"Succesfully created {count} containers\n")
@@ -28,13 +28,13 @@ def choises():
     print("6. Exit\n")
 
 def delete_file(filename):
-    subprocess.run(['rm', '-f', f'{filename}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    subprocess.run(['rm', '-f', f'../{sys.argv[1]}/{filename}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 def create_inventory(filename, ip_s, group):
-    delete_file(f"{filename}.yml")
+    delete_file(f"../{sys.argv[1]}/{filename}.yml")
     login = "root"
     password = "123456789"
-    with open(f"{filename}.yml", 'a') as file:
+    with open(f"../{sys.argv[1]}/{filename}.yml", 'a') as file:
         file.write(f"{group}:\n")
         file.write("  hosts:\n")
         for idx, i in enumerate(ip_s):
@@ -65,7 +65,7 @@ def question_answer_module_two():
 def delete_inventory_playbook_tmp():
     inventory = input("Write down the name of the inventory... ")
     playbook = input("Write down the name of the playbook... ")
-    subprocess.run(['rm', '-f', f'{inventory}.yml', f'{playbook}.yml', 'file.tmp'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    subprocess.run(['rm', '-f', f'../{sys.argv[1]}/{inventory}.yml', f'../{sys.argv[1]}/{playbook}.yml', 'file.tmp'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 if __name__ == "__main__":
     choises()
